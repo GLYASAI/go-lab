ab4a01f87e64417f9625dda43ef66b8a3a99d594
GLYASAI <chaoschunqfeijg@163.com>
2018-12-07 10:42:51 +0800

    print msg into file

--- udp/server/main.go
+++ udp/server/main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net"
+	"os"
+	"path"
 )
 
 func main() {
+	output := "/run/output"
+	if !isExists(output) {
+		err := os.MkdirAll(path.Dir(output), 0777)
+		if err != nil {
+			panic(err)
+		}
+	}
 	listener, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.ParseIP("0.0.0.0"), Port: 38080})
 	if err != nil {
 		fmt.Println(err)
@@ -19,5 +29,20 @@ func main() {
 			fmt.Printf("error during read: %s", err)
 		}
 		fmt.Printf("<%s> %s\n", remoteAddr, data[:n])
+		err = ioutil.WriteFile(output, data[:n], 0644)
+		if err != nil {
+			panic(err)
+		}
 	}
+}
+
+func isExists(f string) bool {
+	_, err := os.Stat(f)
+	if err != nil {
+		if os.IsExist(err) {
+			return true
+		}
+		return false
+	}
+	return true
 }
